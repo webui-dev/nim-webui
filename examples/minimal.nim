b8ef2803231e0fafc3b5391042a6e579cb504a55
t-h-rk-turner <71654487+t-h-rk-turner@users.noreply.github.com>
2023-03-11 18:28:20 -0500

    add comments

--- examples/minimal.nim
+++ examples/minimal.nim
@@ -1,6 +1,6 @@
 import webui
 
-let my_window = newWindow()
-my_window.show("<html>Hello</html>")
+let window = newWindow() # Create a new Window
+window.show("<html>Hello</html>") # Show the window with html content
 
-wait()
+wait() # Wait until the window gets closed
