fafc8c72f1c7b442f4f31a0d7f8391fbe7247e02
neroist <pmpiga1@gmail.com>
2023-06-23 17:25:11 -0400

    add `js.nim`

---
+++ webui/js.nim
@@ -0,0 +1,83 @@
+import std/htmlparser
+import std/tempfiles
+import std/strutils
+import std/xmltree
+import std/macros
+import std/osproc
+import std/os
+
+#import ../webui
+
+#let (file, path) = createTempFile("tmpnim_", "webui.nim")
+#file.write quote(body).astToStr
+#file.close()
+#echo path
+
+var code*: seq[string]
+
+macro defJsProcImpl*(name: static string, body: untyped): string = 
+  var fn = newProc(ident(name), body=body)
+
+  fn.addPragma(ident"exportc")
+
+  return newStrLitNode(repr fn)
+
+macro jsCodeImpl*(body: untyped): string =
+  if body.kind() == nnkProcDef:
+    body.addPragma(ident"exportc")
+
+  return newStrLitNode(repr body)
+
+template jsCode*(body: untyped) = 
+  code.add jsCodeImpl(body)
+
+template defJsProc*(name: static string, body: untyped) =
+  code.add defJsProcImpl(name, body)
+
+# TODO fix approach? parsing the html isnt the best
+proc collectJs*(html: string): string = 
+  let collectedCode = code.join()
+
+  let (file, path) = createTempFile("tmpnim_", "webui.nim")
+  file.write collectedCode
+  file.close()
+
+  echo path
+
+  discard execProcess(
+    "nim",
+    getCurrentDir(),
+    ["js", "-d:release", "-d:danger", "--opt:size", path],
+    options={poUsePath, poStdErrToStdOut}
+    )
+
+  let js = readFile(path.changeFileExt("js"))
+
+  #[
+  var 
+    xml = if html.fileExists(): loadHtml(html)
+          else: parseHtml(html)
+            
+    node = newElement("script")
+  
+  node.attrs = toXmlAttributes {"defer": "true"}
+  node.add newVerbatimText js
+
+  xml[1][1].add node
+
+  writeFile("z.html", $xml)
+
+  return $xml
+  ]#
+  
+when isMainModule:
+  defJsProc("conversation"):
+    echo "i am made of love"
+
+  proc thisorthat(who: string, where: int) {.jsCode.} = 
+    let this = where * 5
+
+    echo this
+    echo who, '?'
+
+  echo collectJs("./examples/serve_folder/index.html")
