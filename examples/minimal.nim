1abce10f94fed4688f75e3de27d191b0374f1acd
neroist <pmpiga1@gmail.com>
2023-12-30 17:01:52 -0500

    update html in minimal` example

--- examples/minimal.nim
+++ examples/minimal.nim
@@ -1,6 +1,6 @@
 import webui
 
 let window = newWindow() # Create a new Window
-window.show("<html>Hello</html>") # Show the window with html content
+window.show("<html><head><script src='webui.js'></script></head> Hello World ! </html>") # Show the window with html content
 
 wait() # Wait until the window gets closed
