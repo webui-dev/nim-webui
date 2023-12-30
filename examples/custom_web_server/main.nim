0e3c0bf620f0a866b4c131952b94eb071a35a24a
neroist <pmpiga1@gmail.com>
2023-12-30 18:36:20 -0500

    add "custom web server" example

---
+++ examples/custom_web_server/main.nim
@@ -0,0 +1,59 @@
+
+import webui
+
+proc main =
+  # Create new windows
+  let window = newWindow()
+
+  # Bind all events
+  window.bind("") do (e: Event):
+    # This function gets called every time
+    # there is an event
+
+    case e.eventType
+    of EventsConnected:
+      echo "Connected"
+    of EventsDisconnected:
+      echo "Disonnected"
+    #of EventsMouseClick:
+    #  echo "Click"
+    of EventsNavigation:
+      let url = e.getString()
+      echo "Starting navigation to: ", url
+
+      # Because we used `window.bind("")`
+      # WebUI will block all `href` link clicks and sent here instead.
+      # We can then control the behaviour of links as needed.
+
+      e.window.navigate(url)
+    else: 
+      discard
+
+  # Bind HTML elements with Nim function
+  window.bind("my_backend_func") do (e: Event):
+    let
+      number1 = e.getInt(0)
+      number2 = e.getInt(1)
+      number3 = e.getInt(2)
+
+    echo "my_backend_func 1: ", number1 # 123
+    echo "my_backend_func 2: ", number2 # 456
+    echo "my_backend_func 3: ", number3 # 789
+
+  # Set web server network port WebUI should use
+  # this means `webui.js` will be available at:
+  # http://localhost:8081/webui.js
+  window.port = 8081
+
+  # Show a new window and show our custom web server
+  # Assuming the custom web server is running on port
+  # 8080... 
+  window.show("http://localhost:8080")
+
+  # Wait until all windows get closed
+  wait()
+
+  # Free all memory resources (Optional)
+  clean()
+
+main()
---
+++ examples/custom_web_server/server.nim
@@ -0,0 +1,21 @@
+import std/asynchttpserver
+import std/asyncdispatch
+
+proc main {.async.} =
+  var server = newAsyncHttpServer()
+
+  proc cb (req: Request) {.async.} =
+    # echo (req.reqMethod, req.url, req.headers)
+    
+    let headers = {"Content-type": "text/plain; charset=utf-8"}
+    await req.respond(Http200, "Hello World", headers.newHttpHeaders())
+
+  server.listen(Port(8080))
+  
+  while true:
+    if server.shouldAcceptRequest():
+      await server.acceptRequest(cb)
+    else:
+      await sleepAsync(500)
+
+waitFor main()
